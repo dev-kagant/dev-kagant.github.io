938dfb60c2f0a34c690c221bd46347f8c7092b4d
Kerri <gant.kerri@gmail.com>
2021-10-28 13:37:18 -0400

    testing links 6

--- assets/js/util.js
+++ assets/js/util.js
@@ -157,21 +157,21 @@
 						return;
 
 					// Cancel original event.
-					// 	event.preventDefault();
-					// 	event.stopPropagation();
+					event.preventDefault();
+					event.stopPropagation();
 
-					// // Hide panel.
-					// 	$this._hide();
+					// Hide panel.
+					$this._hide();
 
-					// // Redirect to href.
-					// 	window.setTimeout(function() {
+					// Redirect to href.
+					window.setTimeout(function () {
 
-					// 		if (target == '_blank')
-					// 			window.open(href);
-					// 		else
-					// 			window.location.href = href;
+						if (target == '_blank')
+							window.open(href);
+						else
+							window.location.href = href;
 
-					// 	}, config.delay + 10);
+					}, config.delay + 10);
 
 				});
 
